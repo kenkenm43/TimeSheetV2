b4abf517c4795b4e6e0a81efad637b0edfa6b56f
kenkenm43 <kenkenm43@gmail.com>
2024-07-17 14:11:23 +0700

    update

--- client/src/pages/error-page.tsx
+++ client/src/pages/error-page.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  return (
+    <section className="w-full mx-auto">
+      <h1>ไม่มีหน้านี้</h1>
+      <br />
+      <p>คลิกปุ่มกลับเพื่อกลับไปหน้าแรก</p>
+      <div className="flexGrow">
+        <button className="text-blue-700 hover:text-blue-800 " onClick={goBack}>
+          กลับ
+        </button>
+      </div>
+    </section>
+  );
 };
 
 export default ErrorPage;
