42fb6521027b3de82939836efe319a1e0098aa2a
kenkenm43 <kenkenm43@gmail.com>
2024-07-18 12:33:08 +0700

    update

--- server/src/app/middlewares/credentials.ts
+++ server/src/app/middlewares/credentials.ts
@@ -4,7 +4,15 @@ import allowedOrigins from "../../config/allowedOrigins";
 const credentials = (req: any, res: any, next: any) => {
   const origin = req.headers?.origin;
   if (allowedOrigins.includes(origin)) {
-    res.header("Access-Control-Allow-Credentials", true);
+    res.header(
+      "Access-Control-Allow-Credentials",
+      "https://time-sheet-v2.vercel.app"
+    );
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization"
+    );
   }
   next();
 };
